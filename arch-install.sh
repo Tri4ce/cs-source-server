17eeefb1f3efe12362ed55fc6fd4d1191274807e
Justin Garfield <Justin Garfield>
2016-12-28 08:49:26 -0500

    First commit to create repo

---
+++ arch-install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# This script is derived from the forum post at http://forums.srcds.com/viewtopic/18513.
+# It has been modified to work with an 80GB disk and use the UTC timezone for universal
+# time-stamping.
+
+# Create partitions for the boot loader and a / root to hold everything else.
+parted /dev/sda mklabel gpt
+parted /dev/sda mkpart primary ext2 1 3                  # Grub bios-gpt
+parted /dev/sda mkpart primary ext2 3 259                # /boot
+parted /dev/sda mkpart primary ext4 259 100%             # /
+
+# grub needs a small part for it's core.img, otherwise not available with a bios-gpt drive
+# http://www.gnu.org/software/grub/manual/html_node/BIOS-installation.html#BIOS-installation
+parted /dev/sda set 1 bios_grub on
+parted /dev/sda set 2 boot on
+parted /dev/sda name 1 grub
+parted /dev/sda name 2 boot
+parted /dev/sda name 3 root
+
+# Arch parted doesn't have the mkfs or mkpartfs commands
+mkfs.ext2 /dev/sda2
+mkfs.ext4 /dev/sda3
+
+mount /dev/sda3 /mnt
+mkdir /mnt/boot
+mount /dev/sda2 /mnt/boot
+parted /dev/sda print
+
+# Get current list of US mirrors
+curl -o mirrorlist https://www.archlinux.org/mirrorlist/?country=US&protocol=http&ip_version=4
+# Uncomment all mirrors
+sed '/^#\S/ s|#||' -i mirrorlist
+# Rank mirrors and sort them accordingly
+/usr/bin/rankmirrors -n 0 mirrorlist > /etc/pacman.d/mirrorlist
+# pacstrap will copy mirrorlist to our installation
+
+pacstrap /mnt base base-devel
+genfstab -p /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
